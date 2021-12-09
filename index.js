129b950dc3d190b1ae159f69c09a175591746d85
= <rtyenriques@gmail.com>
2021-12-09 01:06:07 -0800

    commit tree 762c44670d833ffa21a082500bf89ad4f12b6bac

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let userData = {
+    name: "Ross",
+    email: "rtyenriques@gmail.com"
+}
+
+let submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+};
+
+fetch("http://localhost:3000/users", submitData)
+  .then(function(response) {
+  return response.json(); 
+  }) .then (function(object) {
+      console.log(object)
+  }) .catch(function(error) {
+      alert("bad news");
+      console.log(error.message);
+  });
